633da342198f691dd5a89f6a507c3e4eb9208986
Edwin Boyko <>
2019-05-30 22:40:36 +0100

    implement index page

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,27 @@
+var blogPostApi = Vue.resource('api/v1/posts/getAll');
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+Vue.component('posts-list', {
+  props: ['posts'],
+  template:'<div><blog-post v-for="post in posts" :key="post.id" :post="post" /></div>'
+})
+var app = new Vue({
+  el: '#app',
+  template: '<posts-list :posts="posts" />',
+  data: {
+    posts: []
+  },
+  created: function() {
+    blogPostApi.get().then(result =>
+        result.json().then(data => data.forEach(post => this.posts.push(post)))
+        )
+  }
+});
